e4b9d7ade271b55b42c053a94dd322bd558ba96f
Examen33 <sereja.buschuev@gmail.com>
2025-02-13 19:37:42 +0300

    Initial commit

---
+++ skript.py
@@ -0,0 +1,78 @@
+import os
+import random
+
+from datacenter.models import (Chastisement, Commendation, Lesson, Mark,
+                               Schoolkid)
+from dotenv import load_dotenv
+
+PRAISES = [
+    'Молодец!', 'Отлично!', 'Хорошо!', 'Гораздо лучше, чем я ожидал!', 'Ты меня приятно удивил!', 'Великолепно!',
+    'Прекрасно!', 'Ты меня очень обрадовал!', 'Именно этого я давно ждал от тебя!', 'Сказано здорово – просто и ясно!',
+    'Ты, как всегда, точен!', 'Очень хороший ответ!', 'Талантливо!', 'Ты сегодня прыгнул выше головы!', 'Я поражен!',
+    'Уже существенно лучше!', 'Потрясающе!', 'Замечательно!', 'Прекрасное начало!', 'Так держать!',
+    'Ты на верном пути!',
+    'Здорово!', 'Это как раз то, что нужно!', 'Я тобой горжусь!', 'С каждым разом у тебя получается всё лучше!',
+    'Мы с тобой не зря поработали!', 'Я вижу, как ты стараешься!', 'Ты растешь над собой!',
+    'Ты многое сделал, я это вижу!',
+    'Теперь у тебя точно все получится!',
+]
+
+
+def fix_marks(schoolkid):
+    Mark.objects.filter(schoolkid=schoolkid, points__in=[2, 3]).update(points=5)
+
+
+def remove_chastisements(schoolkid):
+    bad_remarks = Chastisement.objects.filter(schoolkid=schoolkid)
+    bad_remarks.delete()
+
+
+def create_commendation(schoolkid, lesson):
+    text_praise = random.choice(PRAISES)
+
+    subject_at_school = Lesson.objects.filter(
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter,
+        subject__title=lesson
+    ).first()
+
+    if subject_at_school:
+        Commendation.objects.create(
+            text=text_praise,
+            created=subject_at_school.date,
+            schoolkid=schoolkid,
+            subject=subject_at_school.subject,
+            teacher=subject_at_school.teacher
+        )
+
+
+def find_student_by_name(name):
+    try:
+        return Schoolkid.objects.get(full_name=name)
+    except Schoolkid.MultipleObjectsReturned:
+        raise Schoolkid.MultipleObjectsReturned
+    except Schoolkid.DoesNotExist:
+        raise Schoolkid.DoesNotExist
+
+
+def main():
+    load_dotenv()
+
+    name = os.getenv('NAME')
+    lesson = os.getenv('LESSON')
+
+    if not name or not lesson:
+        print("Не указаны переменные окружения.")
+        return
+
+    try:
+        student = find_student_by_name(name)
+        fix_marks(student)
+        remove_chastisements(student)
+        create_commendation(student, lesson)
+    except Exception as error:
+        raise ValueError(f"Произошла ошибка: {error}")
+
+
+if __name__ == '__main__':
+    main()
